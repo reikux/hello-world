0e9ed242d0840a44c8a3f3c5180b12a43def0955
reikux <115341451+reikux@users.noreply.github.com>
2022-10-08 16:24:23 +0200

    Add files via upload

---
+++ zadanie 1.1.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Kamil\n";
+    cout<<"s28839\n";
+    cout<<"PJATK\n";
+    
+;
+
+    return 0;
+}
