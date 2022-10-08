06009e7c8dd7c7047faeb69ae73ed6fa652ed812
reikux <115341451+reikux@users.noreply.github.com>
2022-10-08 17:00:52 +0200

    Add files via upload

---
+++ zadanie 1.3.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+   int liczba;
+   
+   cout <<"wprowadz liczbe" <<endl;
+   cin>>liczba;
+   
+   if (liczba %2==0)
+   {
+   cout <<"Liczba jest dodatnia" <<endl;}
+
+   else{
+   cout <<"wpisana liczba jest nieparzysta" <<endl;
+       
+   }
+   
+   return(0);
+    
+}
+   
+   
+   
+   
+   
+   
+   
+
+ 
